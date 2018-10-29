c8366ff23d0a610da2049519004075bf5569ad7e
Xingyu Yang <yangxingyu18@sdjzu.edu.cn>
2018-10-29 13:44:03 +0800

    修改了命名，增加了迷宫程序的工程，实现读取功能

---
+++ Maze/Maze.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Maze
+{
+    class Maze
+    {
+        static void Main(string[] args)
+        {
+            string strLine;
+            try
+            {
+                FileStream aFile = new FileStream("maze.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+                strLine = sr.ReadLine();
+                //Read data in line by line 这个兄台看的懂吧~一行一行的读取
+                while (strLine != null)
+                {
+                    Console.WriteLine(strLine);
+                    strLine = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine("An IOException has been thrown!");
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
