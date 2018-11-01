145ed094f78884f31f812630c2844b2527418f63
Xingyu Yang <yangxingyu18@sdjzu.edu.cn>
2018-11-01 12:17:48 +0800

    创建了一个考眼力小游戏

---
+++ Game_1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Game_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MuBiao = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label34 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Restart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MuBiao
+            // 
+            this.MuBiao.AutoSize = true;
+            this.MuBiao.Location = new System.Drawing.Point(104, 63);
+            this.MuBiao.Name = "MuBiao";
+            this.MuBiao.Size = new System.Drawing.Size(41, 12);
+            this.MuBiao.TabIndex = 0;
+            this.MuBiao.Text = "MuBiao";
+            this.MuBiao.Click += new System.EventHandler(this.MuBiao_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "KaiShi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label34
+            // 
+            this.label34.AutoSize = true;
+            this.label34.Location = new System.Drawing.Point(210, 22);
+            this.label34.Name = "label34";
+            this.label34.Size = new System.Drawing.Size(35, 12);
+            this.label34.TabIndex = 34;
+            this.label34.Text = "Time:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(115, 17);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 35;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 461);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.label34);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MuBiao);
+            this.Location = new System.Drawing.Point(150, 150);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MuBiao;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label34;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Restart;
+    }
+}
+
---
+++ Game_1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Game_1
+{
+    public partial class Form1 : Form
+    {
+        public Random ram = new Random();
+        public char[] Zifu = {
+            '1', 'I', 'I','1',
+            '0', 'o', '0', 'O', 'o', 'O' , 'o', '0', 'O', '0', 'O', 'o',
+            '5', 's', 's', '5', '5', 'S' , 'S', '5', 'S', 's', 's', 'S',
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void MuBiao_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label34.Text = "time:"+(time/10.0).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int a= ram.Next(0, 14)*2;
+            for (int i = ram.Next(20, 30); i >0 ; i--)
+            {
+                Label lbl = new Label();//声明一个label
+                lbl.Location = new System.Drawing.Point(ram.Next(20,430), ram.Next(100, 430));//设置位置
+                lbl.Size = new Size(20, 30);//设置大小
+                lbl.Text = Zifu[a].ToString();//设置Text值
+                this.Controls.Add(lbl);//在当前窗体上添加这个label控件
+            }
+            MuBiao.Location = new System.Drawing.Point(ram.Next(20, 430), ram.Next(100, 430));
+            MuBiao.Text = Zifu[a + 1].ToString();
+            timer1.Enabled = true;
+        }
+        public Int32 time = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+            Application.Exit();
+            Application.Restart();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
